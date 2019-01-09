56f86b2429c4a94df4d944055a3cd47608f2241d
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-01-09 03:57:14 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -44,9 +44,5 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.chomp
   input_to_index(input)
-  if valid_move?(board, index) == true
-    move(board, index)
-  else
-    
-  end
+
 end
